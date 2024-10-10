45daf9cda162764c4698bf98dd628404a947d329
Ed Kom <execrot@airkod.com>
2024-10-10 17:26:12 +0300

    Update

--- helper/data.sh
+++ helper/data.sh
@@ -2,4 +2,4 @@ rm -rf /var/www/fs/www/storage;
 cp -r data/storage /var/www/fs/www/storage;
 chown -R www-data:www-data /var/www/fs/www/storage;
 
-mongorestore --db $dbName data/db
+mongorestore --db $dbName data/db;
